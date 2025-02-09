9d4ad97fc6c9986d36ab44ab4277754f49d15729
joonseojung <joonseo1227@gmail.com>
2025-02-09 22:37:18 +0900

    fix: CI/CD

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import {useState} from "react";
 import InputForm from "./components/InputForm";
 import ResultDisplay from "./components/ResultDisplay";
-import { generateSentence } from "./utils/textGenerator";
+import {generateSentence} from "./utils/textGenerator";
+import {BrowserRouter as Router} from "react-router-dom";
 
 export default function App() {
     const [generatedText, setGeneratedText] = useState("");
@@ -11,10 +12,12 @@ export default function App() {
     };
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-            <h1 className="text-3xl font-bold mb-4">🍔 햄부기 생성기 🍔</h1>
-            <InputForm onGenerate={handleGenerate} />
-            <ResultDisplay generatedText={generatedText} />
-        </div>
+        <Router basename={import.meta.env.VITE_PUBLIC_URL || "/"}>
+            <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+                <h1 className="text-3xl font-bold mb-4">🍔 햄부기 생성기 🍔</h1>
+                <InputForm onGenerate={handleGenerate}/>
+                <ResultDisplay generatedText={generatedText}/>
+            </div>
+        </Router>
     );
 }
