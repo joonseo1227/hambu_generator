cac455a86d7e7c0df1fb5ccf7aad600dbe778871
joonseojung <joonseo1227@gmail.com>
2025-02-09 22:15:16 +0900

    feat: CI/CD

--- src/utils/textGenerator.jsx
+++ src/utils/textGenerator.jsx
@@ -3,16 +3,15 @@ export const transformWord = (word) => {
 };
 
 export const transformAlternative = (word) => {
-    if (word.length > 2) {
-        return word.substring(0, 2) + "북"; // 음식 이름의 앞 두 글자를 활용
-    } else {
-        return word + "북"; // 단어가 짧으면 그대로 + '북' 추가
+    if (word.length > 0) {
+        return word[0] + "북"; // 첫 글자 + '북'
     }
+    return "햄북"; // 예외 처리 (빈 문자열 방지)
 };
 
 export const generateSentence = (foodName) => {
     const transformed = transformWord(foodName);
     const transformedAlt = transformAlternative(foodName); // 자연스러운 '햄북' 변형
 
-    return `${transformedAlt} ${transformedAlt}어 ${transformedAlt}스딱스 함부르크${transformed}가우가 ${transformed}햄부거 ${transformed}가티${transformed}온앤 온을 차려오거라. ${transformedAlt} ${transformedAlt}어 ${transformedAlt}스딱스 함부르크${transformed}가우가 ${transformed}햄부거 ${transformed}가티${transformed}온앤 온을 차려오라고 하지 않앗느냐.`;
+    return `${transformed}기${transformedAlt} ${transformedAlt}어 ${transformedAlt}스딱스 함부르크${transformed}가우가 ${transformedAlt}${transformed}거 ${transformed}가티${transformed}온앤 온을 차려오거라. ${transformedAlt} ${transformedAlt}어 ${transformedAlt}스딱스 함부르크${transformed}가우가 ${transformed}햄부거 ${transformed}가티${transformed}온앤 온을 차려오라고 하지 않앗느냐.`;
 };
